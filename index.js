d021511206367897f8e8914b392aa8867756cc24
ELIUS <eliuswanyoro@gmail.com>
2022-08-08 06:46:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+   main.remove();
+
+   //document.createElement('h1#victory')
+    const newHeader = document.createElement("h1");
+
+    newHeader.id= 'victory';
+
+    
+
+    newHeader.innerHTML="YOUR-NAME is the champion";
+
+
